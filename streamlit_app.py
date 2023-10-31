b358633f0b6ec5b8d3a0a4903e3824a9879999db
Aniket  Hend <119605156+AniketHend@users.noreply.github.com>
2023-10-31 21:03:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 # data = pd.read_csv("newdata.csv",sep="\t")
 # try:
-data = pd.read_csv("newdata.csv", sep="\t")
+data = pd.read_csv("newdata.csv", sep="\t",encoding='utf-8')
 # except Exception as e:
 #     print(f"An error occurred: {str(e)}")
 
