37faa9e7d26aea1ebedc7399514d017adbae61d6
Aniket  Hend <119605156+AniketHend@users.noreply.github.com>
2023-11-01 09:00:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,35 +1,18 @@
 import streamlit as st
 import pandas as pd
 import random
-from sklearn.metrics.pairwise import cosine_similarity
 
 # Load data
 data = pd.read_csv("newdata.csv")
 
-# Create interaction matrix and find similarity
-interaction_matrix = data.pivot_table(index='User ID', columns='book id', values='Rating', fill_value=0)
-product_similarity = cosine_similarity(interaction_matrix.T)
-
-# Function to get book recommendations based on book name and author
-def get_recommendations(user_id, book_name, author, interaction_matrix, product_similarity, num_recommendations=2):
-    if user_id not in interaction_matrix.index:
+# Function to get popular books in the same genre
+def get_recommendations(book_name, genre, num_recommendations=2):
+    filtered_books = data[(data['genre'] == genre) & (data['book name'] != book_name)]
+    if filtered_books.empty:
         return None
     
-    user_interactions = interaction_matrix.loc[user_id].values
-    similar_scores = product_similarity.dot(user_interactions)
-    recommended_indices = similar_scores.argsort()[-num_recommendations:][::-1]
-    recommended_books = interaction_matrix.columns[recommended_indices]
-    
-    # Filter recommended books by book name and author
-    filtered_books = filter_by_book_name_and_author(recommended_books, book_name, author)
-    
-    return filtered_books
-
-# Function to filter recommended books by book name and author
-def filter_by_book_name_and_author(books, book_name, author):
-    filtered_books = data[data['book name'] == book_name]
-    filtered_books = filtered_books[filtered_books['author'] == author]
-    return filtered_books['book id']
+    popular_recommendations = filtered_books.nlargest(num_recommendations, 'Rating')[['book name', 'genre']]
+    return popular_recommendations
 
 # Streamlit app
 def main():
@@ -43,25 +26,16 @@ def main():
     st.markdown("Discover personalized book recommendations.")
     
     # User input
-    user_id = st.number_input("Enter User ID", min_value=1, max_value=1000)
     book_name = st.selectbox("Select Book Name", data['book name'].unique())
-    author = st.selectbox("Select Author", data['author'].unique())
+    genre = st.selectbox("Select Genre", data['genre'].unique())
    
     # Recommendation button
     if st.button("Get Recommendations"):
-        recommendations = get_recommendations(user_id, book_name, author, interaction_matrix, product_similarity)
+        recommendations = get_recommendations(book_name, genre)
         
-        if recommendations is not None and len(recommendations) > 0:
-            random_recommendations = random.sample(list(recommendations), min(2, len(recommendations)))
-        
-            # Display recommended books
+        if recommendations is not None and not recommendations.empty:
             st.subheader("Recommended Books:")
-            recommended_books_info = data[data['book id'].isin(random_recommendations)][['book id', 'book name', 'author', 'genre', 'Price', 'Rating', 'publication', 'number of pages']]
-            
-            if not recommended_books_info.empty:
-                st.table(recommended_books_info)
-            else:
-                st.subheader("No Recommendations Found")
+            st.table(recommendations)
         else:
             st.subheader("No Recommendations Found")
 
