83c88cbfa4b1a48616d89d72a126168bc334436b
Aniket  Hend <119605156+AniketHend@users.noreply.github.com>
2023-11-01 08:15:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,75 +3,67 @@ import pandas as pd
 import random
 from sklearn.metrics.pairwise import cosine_similarity
 
-import chardet
-
-with open("newdata.csv", 'rb') as f:
-    result = chardet.detect(f.read())
-# data = pd.read_csv("newdata.csv",sep="\t")
-# try:
-# data = pd.read_csv("newdata.csv", sep="\t",encoding='utf-8')
-# except Exception as e:
-#     print(f"An error occurred: {str(e)}")
-data = pd.read_csv("newdata.csv", sep="\t", encoding=result['encoding'])
+# Load data
+data = pd.read_csv("newdata.csv")
 
 # Create interaction matrix and find similarity
 interaction_matrix = data.pivot_table(index='User ID', columns='book id', values='Rating', fill_value=0)
 product_similarity = cosine_similarity(interaction_matrix.T)
 
-# Function to get product recommendations based on product name and category
-def get_recommendations(user_id, product_name, category, interaction_matrix, product_similarity, num_recommendations=50):
+# Function to get book recommendations based on book name and author
+def get_recommendations(user_id, book_name, author, interaction_matrix, product_similarity, num_recommendations=2):
+    if user_id not in interaction_matrix.index:
+        return None
+    
     user_interactions = interaction_matrix.loc[user_id].values
     similar_scores = product_similarity.dot(user_interactions)
     recommended_indices = similar_scores.argsort()[-num_recommendations:][::-1]
-    recommended_products = interaction_matrix.columns[recommended_indices]
+    recommended_books = interaction_matrix.columns[recommended_indices]
     
-    # Filter recommended products by product name and category
-    filtered_products = filter_by_product_name_and_category(recommended_products, product_name, category)
+    # Filter recommended books by book name and author
+    filtered_books = filter_by_book_name_and_author(recommended_books, book_name, author)
     
-    return filtered_products
+    return filtered_books
 
-# Function to filter recommended products by product name and category
-def filter_by_product_name_and_category(products, product_name, category):
-    filtered_products = data[data['book name'] == product_name]
-    filtered_products = filtered_products[filtered_products['author'] == category]
-    return filtered_products
+# Function to filter recommended books by book name and author
+def filter_by_book_name_and_author(books, book_name, author):
+    filtered_books = data[data['book name'] == book_name]
+    filtered_books = filtered_books[filtered_books['author'] == author]
+    return filtered_books['book id']
 
 # Streamlit app
 def main():
     
     st.set_page_config(
-        page_title="Fashion Product Recommender",
-        page_icon="🛍️",
-        
+        page_title="Book Recommender",
+        page_icon="📚",
     )
 
-    
-    st.title("Fashion Product Recommender")
-    st.markdown("Discover personalized fashion product recommendations.")
+    st.title("Book Recommender")
+    st.markdown("Discover personalized book recommendations.")
     
     # User input
     user_id = st.number_input("Enter User ID", min_value=1, max_value=1000)
-    product_name = st.selectbox("Select Product Name", data['book name'].unique())
-    category = st.selectbox("Select Category", data['author'].unique())
+    book_name = st.selectbox("Select Book Name", data['book name'].unique())
+    author = st.selectbox("Select Author", data['author'].unique())
    
     # Recommendation button
     if st.button("Get Recommendations"):
-        recommendations = get_recommendations(user_id, product_name, category, interaction_matrix, product_similarity)
+        recommendations = get_recommendations(user_id, book_name, author, interaction_matrix, product_similarity)
         
-        if len(recommendations) > 10:
-            random_recommendations = random.sample(list(recommendations['book id']), 10)
+        if recommendations is not None and len(recommendations) > 0:
+            random_recommendations = random.sample(list(recommendations), min(2, len(recommendations)))
+        
+            # Display recommended books
+            st.subheader("Recommended Books:")
+            recommended_books_info = data[data['book id'].isin(random_recommendations)][['book id', 'book name', 'author', 'genre', 'Price', 'Rating', 'publication', 'number of pages']]
+            
+            if not recommended_books_info.empty:
+                st.table(recommended_books_info)
+            else:
+                st.subheader("No Recommendations Found")
         else:
-            random_recommendations = list(recommendations['book id'])
-        
-        # Display recommended products
-        st.subheader("Recommended Products:")
-        recommended_products_info = data[data['book id'].isin(random_recommendations)][['book id', 'book name', 'author', 'genre', 'publication']]
-        st.table(recommended_products_info)
-        
-        # Display user's history
-        st.subheader("User History:")
-        user_products_info = data[data['User ID'] == user_id][['book name', 'author', 'genre', 'publication']].drop_duplicates()
-        st.table(user_products_info)
+            st.subheader("No Recommendations Found")
 
 if __name__ == "__main__":
     main()
